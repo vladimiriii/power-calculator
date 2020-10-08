7ffb4480bb4abd0f8568bbb882e173e1f5cec5a8
vladimiriii <brett.romero@hotmail.com>
2020-10-08 13:03:27 +0200

    Changed 'related' to 'paired'

--- app/static/script/custom/testMap.js
+++ app/static/script/custom/testMap.js
@@ -5,7 +5,7 @@ const testMap = [
         "url": "./t-test-one-sample",
         "attributes": {
             "groups": ["1"],
-            "independence": ["related", "independent", "na"],
+            "independence": ["paired", "independent", "na"],
             "normality": ["yes"],
             "equal-variance": ["yes", "no", "na"]
         }
@@ -21,11 +21,11 @@ const testMap = [
         }
     },
     {
-        "name": "t-test with two related samples",
-        "url": "./t-test-two-related-samples",
+        "name": "t-test with paired samples",
+        "url": "./t-test-paired-samples",
         "attributes": {
             "groups": ["2"],
-            "independence": ["related"],
+            "independence": ["paired"],
             "normality": ["yes"],
             "equal-variance": ["yes"]
         }
