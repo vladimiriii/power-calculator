b3bf7d617f36c3ab982be8846bf1d25e58da8b7c
vladimiriii <brett.romero@hey.com>
2021-01-11 13:54:31 +1000

    Updating test map

--- app/static/script/custom/testMap.js
+++ app/static/script/custom/testMap.js
@@ -1,58 +1,69 @@
 const recommenderFields = ["groups", "independence", "normality", "equal-variance"]
 const testMap = [
     {
-        "name": "t-test with one sample",
+        "name": "z-test – One sample",
+        "url": "./tests/z-test-one-sample",
+        "status": "development",
+        "attributes": {
+            "groups": ["1"],
+            "independence": ["na"],
+            "normality": ["yes"],
+            "known-variance": ["yes"]
+        }
+    },
+    {
+        "name": "z-test – Independent samples",
+        "url": "./tests/z-test-independent-samples",
+        "status": "development",
+        "attributes": {
+            "groups": ["2"],
+            "independence": ["independent"],
+            "normality": ["yes"],
+            "known-variance": ["yes"]
+        }
+    },
+    {
+        "name": "z-test – Paired samples",
+        "url": "./tests/z-test-independent-samples",
+        "status": "development",
+        "attributes": {
+            "groups": ["2"],
+            "independence": ["independent"],
+            "normality": ["yes"],
+            "known-variance": ["yes"]
+        }
+    },
+    {
+        "name": "t-test – One sample",
         "url": "./tests/t-test-one-sample",
         "status": "development",
         "attributes": {
             "groups": ["1"],
             "independence": ["paired", "independent", "na"],
             "normality": ["yes"],
-            "equal-variance": ["yes", "no", "na"]
+            "known-variance": ["no"]
         }
     },
     {
-        "name": "t-test with two independent samples",
+        "name": "t-test – Independent samples",
         "url": "./tests/t-test-independent-samples",
         "status": "available",
         "attributes": {
             "groups": ["2"],
             "independence": ["independent"],
             "normality": ["yes"],
-            "equal-variance": ["yes"]
+            "known-variance": ["no"]
         }
     },
     {
-        "name": "t-test with paired samples",
+        "name": "t-test – Paired samples",
         "url": "./tests/t-test-paired-samples",
         "status": "development",
         "attributes": {
             "groups": ["2"],
             "independence": ["paired"],
             "normality": ["yes"],
-            "equal-variance": ["yes"]
-        }
-    },
-    {
-        "name": "t-test with two independent samples and unequal variance",
-        "url": "./tests/t-test-independent-samples",
-        "status": "available",
-        "attributes": {
-            "groups": ["2"],
-            "independence": ["independent"],
-            "normality": ["yes"],
-            "equal-variance": ["no"]
-        }
-    },
-    {
-        "name": "Bootstrap t-test with two independent samples",
-        "url": "./tests/bootstrap-t-test-two-independent-samples",
-        "status": "development",
-        "attributes": {
-            "groups": ["2"],
-            "independence": ["independent"],
-            "normality": ["yes", "no"],
-            "equal-variance": ["yes", "no"]
+            "known-variance": ["no"]
         }
     },
     {
@@ -75,6 +86,50 @@ const testMap = [
             "independence": ["independent"],
             "normality": ["yes"],
             "equal-variance": ["yes", "no"]
+        }
+    },
+    {
+        "name": "Chi-Squared χ<sup>2</sup> test",
+        "url": "./tests/chi-squared",
+        "status": "development",
+        "attributes": {
+            "groups": ["2"],
+            "independence": ["independent"],
+            "normality": ["no"],
+            "equal-variance": ["no"]
+        }
+    },
+    {
+        "name": "Mann-Whitney test",
+        "url": "./tests/mann-whitney-test",
+        "status": "development",
+        "attributes": {
+            "groups": ["2"],
+            "independence": ["independent"],
+            "normality": ["no"],
+            "known-variance": ["no"]
+        }
+    },
+    {
+        "name": "Wilcoxon Signed Rank test",
+        "url": "./tests/wilcoxon-signed-rank-test",
+        "status": "development",
+        "attributes": {
+            "groups": ["2"],
+            "independence": ["paired"],
+            "normality": ["no"],
+            "equal-variance": ["no"]
+        }
+    },
+    {
+        "name": "Kruskal Wallis test",
+        "url": "./tests/kruskal-wallis-test",
+        "status": "development",
+        "attributes": {
+            "groups": ["3+"],
+            "independence": ["independent"],
+            "normality": ["no"],
+            "equal-variance": ["no"]
         }
     }
 ]
